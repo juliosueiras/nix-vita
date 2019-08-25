58cee720943d81e91a9d62d068d883b25789d48e
Julio Tain Sueiras <juliosueiras@gmail.com>
2019-08-24 23:34:40 -0400

    Rename sign_np to signnp

--- packages/all-packages.nix
+++ packages/all-packages.nix
@@ -5,7 +5,7 @@ with pkgs;
 {
   psvpfsparser = callPackage ./psvpfsparser/default.nix {};
   psvimgtools = callPackage ./psvimgtools/default.nix {};
-  sign_np = callPackage ./sign_np/default.nix {};
+  signnp = callPackage ./sign_np/default.nix {};
   chovy-gen = callPackage ./chovy-gen/default.nix {};
   libkirk = callPackage ./kirk_engine/default.nix {};
 }
